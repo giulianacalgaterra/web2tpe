a3aa196c80fdda46ed4e2fbe38a8b02935c92075
Giuliana Calgaterra <giulianacalgaterra@gmail.com>
2021-10-06 15:19:37 -0300

    agrego cosas en controller y model, htaccess y route

---
+++ VeterinariaController/VeterinariaController.php
@@ -0,0 +1,21 @@
+<?php
+require_once "./VeterinariaModel/VeterinariaModel.php";
+//require_once "./VeterinariaView/VeterinariaView.php";
+
+class VeterinariaController{
+
+    private $model;
+    private $view;
+
+    function __construct(){
+        $this->model = new VeterinariaModel();
+    //$this->view = new VeterinariaView();
+    }
+
+    function showHome(){
+        $clientes = $this->model->getClientes();
+        $mascotas = $this->model->getMascotas();
+        //$this->view->showClientes($clientes, $mascotas);
+    }
+    
+}
---
+++ VeterinariaModel/VeterinariaModel.php
@@ -0,0 +1,44 @@
+<?php
+
+class VeterinariaModel{
+
+    private $db;
+    function __construct(){
+        $this->db = new PDO('mysql:host=localhost;'.'dbname=veterinaria;charset=utf8', 'root', 'giuliana1492');
+    }
+    //trae toda la tabla clientes
+    function getClientes(){
+        $sentencia = $this->db->prepare( "select * from clientes c"); 
+        $sentencia->execute();
+        $tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+        return $tareas;
+    }
+    
+    //mostrar un cliente
+
+    //agregar un cliente
+    function isertCliente($nombre, $apellido, $dni){
+        $sentencia = $this->db->prepare("INSERT INTO clientes (nombre, apellido, dni) VALUES (?,?, ?, ?, ?, ?)");
+        $sentencia->execute(array($nombre,$apellido,$dni));
+    }
+    //eliminar un cliente
+    function deleteClienteFromDB($dni){
+        $sentencia = $this->db->prepare("DELETE FROM clientes WHERE cliente.dni = ?");
+        $sentencia->execute(array($dni));
+    }
+    //modificar un cliente
+    function updateClienteFromDB($nombre, $apellido, $dni){
+        $sentencia = $this->db->prepare("UPDATE  clientes SET nombre=?,apellido=?,dni=? WHERE clientes.dni = ?");
+        $sentencia->execute(array($nombre, $apellido, $dni));
+    }
+
+    //trae toda la tabla mascotas
+    function getMascotas(){
+        $sentencia = $this->db->prepare( "select * from mascotas m");
+        $sentencia->execute();
+        $tareas = $sentencia->fetchAll(PDO::FETCH_OBJ);
+        return $tareas;
+    }
+    
+
+}
