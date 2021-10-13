ef3e9bcb56c88d5ad32deec2cdb273ee10055081
Giuliana Calgaterra <giulianacalgaterra@gmail.com>
2021-10-12 22:35:41 -0300

    mas cambios

--- templates/showMascotaYsusDatos.tpl
+++ templates/showMascotaYsusDatos.tpl
@@ -16,14 +16,14 @@
                 </tr>
             </thead>
             <tbody>
-                {foreach from=$todasLasMascotas item=$Mascotas}
+                {foreach from=$todasLasMascotas item=$Mascota}
             <tr>
                 
-                <td colspan="2">{$Mascotas->id}</td>
-                <td colspan="2">{$Mascotas->nombre_mascota}</td>
-                <td colspan="2">{$Mascotas->dni_cliente}</td>
-                <td colspan="2">{$Mascotas->fecha_de_alta}</td>
-                <td colspan="2">{$Mascotas->fecha_de_actualizacion}</td>
+                <td colspan="2">{$Mascota->id}</td>
+                <td colspan="2">{$Mascota->nombre}</td>
+                <td colspan="2">{$Mascota->dni_cliente}</td>
+                <td colspan="2">{$Mascota->fecha_de_alta}</td>
+                <td colspan="2">{$Mascota->fecha_de_actualizacion}</td>
                 
             </tr>   
                 {/foreach} 
--- templates_c/ef48d7386d025ec7b28440ceb6e8d4e186f769ad_0.file.showMascotaYsusDatos.tpl.php
+++ templates_c/ef48d7386d025ec7b28440ceb6e8d4e186f769ad_0.file.showMascotaYsusDatos.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-10-13 03:07:24
+/* Smarty version 3.1.39, created on 2021-10-13 03:17:19
   from 'D:\xampp\htdocs\web2tpe\templates\showMascotaYsusDatos.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6166314cb79607_34084229',
+  'unifunc' => 'content_6166339fe5d6d0_91351547',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     'ef48d7386d025ec7b28440ceb6e8d4e186f769ad' => 
     array (
       0 => 'D:\\xampp\\htdocs\\web2tpe\\templates\\showMascotaYsusDatos.tpl',
-      1 => 1634087237,
+      1 => 1634087836,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/footer.tpl' => 1,
   ),
 ),false)) {
-function content_6166314cb79607_34084229 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6166339fe5d6d0_91351547 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:templates/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
@@ -43,22 +43,22 @@ $_smarty_tpl->_subTemplateRender('file:templates/header.tpl', $_smarty_tpl->cach
             </thead>
             <tbody>
                 <?php
-$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['todasLasMascotas']->value, 'Mascotas');
-$_smarty_tpl->tpl_vars['Mascotas']->do_else = true;
-if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['Mascotas']->value) {
-$_smarty_tpl->tpl_vars['Mascotas']->do_else = false;
+$_from = $_smarty_tpl->smarty->ext->_foreach->init($_smarty_tpl, $_smarty_tpl->tpl_vars['todasLasMascotas']->value, 'Mascota');
+$_smarty_tpl->tpl_vars['Mascota']->do_else = true;
+if ($_from !== null) foreach ($_from as $_smarty_tpl->tpl_vars['Mascota']->value) {
+$_smarty_tpl->tpl_vars['Mascota']->do_else = false;
 ?>
             <tr>
                 
-                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascotas']->value->id;?>
+                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascota']->value->id;?>
 </td>
-                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascotas']->value->nombre_mascota;?>
+                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascota']->value->nombre;?>
 </td>
-                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascotas']->value->dni_cliente;?>
+                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascota']->value->dni_cliente;?>
 </td>
-                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascotas']->value->fecha_de_alta;?>
+                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascota']->value->fecha_de_alta;?>
 </td>
-                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascotas']->value->fecha_de_actualizacion;?>
+                <td colspan="2"><?php echo $_smarty_tpl->tpl_vars['Mascota']->value->fecha_de_actualizacion;?>
 </td>
                 
             </tr>   
