6b92f3248ee48304fee2b25f84c2b006fca1c977
Giuliana Calgaterra <giulianacalgaterra@gmail.com>
2021-10-14 22:37:57 -0300

    pavadas

--- templates_c/4ac62b88f561d8b81f532591114e9825479d0217_0.file.detalleCliente.tpl.php
+++ templates_c/4ac62b88f561d8b81f532591114e9825479d0217_0.file.detalleCliente.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-10-15 03:13:05
+/* Smarty version 3.1.39, created on 2021-10-15 03:35:00
   from 'D:\xampp\htdocs\web2tpe\templates\detalleCliente.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_6168d5a14fa2d6_31265799',
+  'unifunc' => 'content_6168dac440f681_44143237',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '4ac62b88f561d8b81f532591114e9825479d0217' => 
     array (
       0 => 'D:\\xampp\\htdocs\\web2tpe\\templates\\detalleCliente.tpl',
-      1 => 1634259564,
+      1 => 1634261695,
       2 => 'file',
     ),
   ),
@@ -22,7 +22,7 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:templates/footer.tpl' => 1,
   ),
 ),false)) {
-function content_6168d5a14fa2d6_31265799 (Smarty_Internal_Template $_smarty_tpl) {
+function content_6168dac440f681_44143237 (Smarty_Internal_Template $_smarty_tpl) {
 $_smarty_tpl->_subTemplateRender('file:templates/header.tpl', $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
